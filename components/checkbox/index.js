78b1e35ac994a70bb854e8511db598548a3fe485
Gagandeep Singh <gagan.ddun@gmail.com>
2019-06-19 20:08:35 +1000

    checkbox

---
+++ components/checkbox/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const Checkbox = () => {
+    return (
+        <input type="checkbox" className="custom-checkbox" />
+    )
+};
+
+export default Checkbox;
--- stories/index.js
+++ stories/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Button } from '@storybook/react/demo';
+import Checkbox from '../components/checkbox';
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button>Hello Button</Button>
-  ))
-  .add('with emoji', () => (
-    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  ));   
+storiesOf('Checkbox', module)
+  .add('default', () => (
+    <Checkbox />
+  ));  
